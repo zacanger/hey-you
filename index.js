a59e5775ab3ecbcc13030ece04c336af12ae8fc1
zacanger <zac@zacanger.com>
2016-05-11 18:42:56 -0600

    :card_index: version bump

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const
 , args  = ['-r', '.']
 , arg   = args.concat(process.argv.slice[2])
 
-spawn('./node_modules/.bin/nwbuild', arg, {cwd : __ dirname, stdio : [0, 1, 2]})
+spawn('./node_modules/.bin/nwbuild', arg, {cwd : __dirname, stdio : [0, 1, 2]})
 
