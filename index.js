ea9d64bcc5c7fe5f7224a2e31e061765098e436f
zacanger <zac@zacanger.com>
2016-05-12 00:14:39 -0600

    :ferris_wheel: version bump

--- index.js
+++ index.js
@@ -3,5 +3,10 @@
 const
   spawn = require('child_process').spawn
 , nw    = require('nwjs')
-, child = spawn(nw, ['.'])
+, child = spawn(nw, ['.', {cwd : __dirname}])
+
+
+// var args  = ['-r', '.']
+// args = args.concat(process.argv.slice(2))
+// spawn('./node_modules/.bin/nwbuild', args, {cwd: __dirname, stdio: [0, 1, 2]})
 
